bcd53879d09ae1f989206346693ad749eb307e6f
lucasrpb <LucaBX1789#@!>
2019-08-20 01:05:31 -0300

    First commit.

--- src/main/scala/transaction/Client.scala
+++ src/main/scala/transaction/Client.scala
@@ -1,14 +1,26 @@
 package transaction
 
+import java.nio.ByteBuffer
+import java.util.concurrent.ThreadLocalRandom
+
 import com.datastax.driver.core.Session
+import com.google.protobuf.any.Any
+import com.twitter.finagle.Service
 import com.twitter.util.Future
 import transaction.protocol._
 
+import scala.collection.concurrent.TrieMap
 import scala.concurrent.ExecutionContext
 
-class Client(val id: String, val session: Session)(implicit val ec: ExecutionContext) {
+class Client(val id: String,
+             val session: Session,
+             val coordinators: Map[String, Service[Command, Command]],
+             val npartitions: Int)
+            (implicit val ec: ExecutionContext) {
 
-  val INSERT_TRANSACTION = session.prepare("insert into transactions(id, status, tmp) values(?,?,?);")
+  val rand = ThreadLocalRandom.current()
+
+  val INSERT_TRANSACTION = session.prepare("insert into transactions(id, status, tmp, bin) values(?,?,?,?);")
   val READ_DATA = session.prepare("select * from data where key=?;")
 
   def read(k: String): Future[(String, VersionedValue)] = {
@@ -18,26 +30,48 @@ class Client(val id: String, val session: Session)(implicit val ec: ExecutionCon
     }
   }
 
-  def insertTx(): Future[Boolean] = {
+  def insertTx(tx: Transaction): Future[Boolean] = {
     val tmp = System.currentTimeMillis()
-    session.executeAsync(INSERT_TRANSACTION.bind.setString(0, id).setInt(1, Status.PENDING).setLong(2, tmp))
-      .map(_.wasApplied())
-  }
-
-  def reads(keys: Seq[String], f: (Map[String, VersionedValue]) => Map[String, VersionedValue]): Future[Boolean] = {
-    Future.collect(keys.map{k => read(k)}).flatMap { reads =>
-      val writes = f(reads)
-
-      null
-    }
+    val bytes = Any.pack(tx).toByteArray
+    session.executeAsync(INSERT_TRANSACTION.bind.setString(0, id).setInt(1, Status.PENDING).setLong(2, tmp)
+      .setBytes(3, ByteBuffer.wrap(bytes).flip())).map(_.wasApplied())
   }
 
   def execute(keys: Seq[String])(f: (Map[String, VersionedValue]) => Map[String, VersionedValue]): Future[Boolean] = {
-    insertTx().flatMap { ok =>
-      if(ok){
-        reads(keys, f)
-      } else {
-        Future.value(false)
+    Future.collect(keys.map{k => read(k)}).flatMap { reads =>
+      val writes = f(reads)
+
+      val tx = Transaction(id)
+
+      val requests = TrieMap[String, Enqueue]()
+
+      reads.foreach { case (k, v) =>
+        val p = (k.toInt % npartitions).toString
+
+        requests.get(p) match {
+          case None => requests.put(p, Enqueue(id, Map(k -> v)))
+          case Some(e) => e.addRs(k -> v)
+        }
+      }
+
+      writes.foreach { case (k, v) =>
+        val p = (k.toInt % npartitions).toString
+
+          requests.get(p) match {
+            case None => requests.put(p, Enqueue(id, reads.toMap, Map(k -> v)))
+            case Some(e) => e.addWs(k -> v)
+          }
+      }
+
+      tx.addAllPartitions(requests)
+
+      val c = coordinators(rand.nextInt(0, coordinators.size).toString)
+
+      c(tx).map {
+        _ match {
+          case cmd: Ack => true
+          case cmd: Nack => false
+        }
       }
     }
   }
--- src/test/scala/transaction/MainSpec.scala
+++ src/test/scala/transaction/MainSpec.scala
@@ -11,8 +11,6 @@ class MainSpec extends FlatSpec {
 
   "amount of money" should " be equal after transactions" in {
 
-
-
   }
 
 }
